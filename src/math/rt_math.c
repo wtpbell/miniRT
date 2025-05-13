6352a62d87f6abf693ec6a613249611c2dac3b87
Jose Boon <jboon@student.codam.nl>
2025-05-13 18:21:23 +0200

    Added rt_math.c

---
+++ src/math/rt_math.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   rt_math.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jboon <jboon@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/05/13 13:40:09 by jboon         #+#    #+#                 */
+/*   Updated: 2025/05/13 17:37:20 by jboon         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+float	ft_maxf(float a, float b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+float	ft_signf(float a)
+{
+	if (a < 0)
+		return (-1.0f);
+	return (1.0f);
+}
+
+void	ft_swapf(float *a, float *b)
+{
+	float	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
