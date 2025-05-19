edb4fabf4fa41dc1716aced1d1520f2ef79204a6
Jose Boon <jboon@student.codam.nl>
2025-05-19 14:19:39 +0200

    Added matrix transpose

--- src/render/rt_plane.c
+++ src/render/rt_plane.c
@@ -6,7 +6,7 @@
 /*   By: jboon <jboon@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/19 10:33:19 by jboon         #+#    #+#                 */
-/*   Updated: 2025/05/19 10:57:08 by jboon         ########   odam.nl         */
+/*   Updated: 2025/05/19 11:44:19 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	plane_intersect(t_obj *obj, t_ray *ray, float *dst)
 {
 	float	denom;
 
-	denom = v3f_dot(ray->direction, obj->t.dir);
+	denom = v3f_dot(obj->t.dir, ray->direction);
 	if (denom > FLT_EPI)
 	{
 		*dst = v3f_dot(v3f_sub(obj->t.pos, ray->origin), obj->t.dir) / denom;
