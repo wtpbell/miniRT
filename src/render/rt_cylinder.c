f35e850b82323550006d706370b4e4e150b6b8bb
Jose Boon <jboon@student.codam.nl>
2025-05-27 17:53:27 +0200

    Simplified the check_body and norm

--- src/render/rt_cylinder.c
+++ src/render/rt_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: bewong <bewong@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/17 11:59:52 by bewong        #+#    #+#                 */
-/*   Updated: 2025/05/27 14:52:10 by jboon         ########   odam.nl         */
+/*   Updated: 2025/05/27 17:52:59 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ t_v3f	cylinder_normal(t_obj *obj, t_v3f point)
 				obj->t.to_world)));
 }
 
-static int	intersect_cylinder_disc(float r, float h, t_ray *ray, t_v2f *t_range)
+static int	intersect_cylinder_disc(float r, float h, t_ray *ray, t_v2f *t_lim)
 {
 	float	t;
 	t_v3f	p;
@@ -40,35 +40,28 @@ static int	intersect_cylinder_disc(float r, float h, t_ray *ray, t_v2f *t_range)
 	if (fabsf(ray->direction.y) < FLT_SML)
 		return (0);
 	t = (h - ray->origin.y) / ray->direction.y;
-	if (t > t_range->x && t < t_range->y)
+	if (t > t_lim->x && t < t_lim->y)
 	{
 		p = v3f_add(ray->origin, v3f_scale(ray->direction, t));
 		if ((p.x * p.x + p.z * p.z) <= r * r)
-			return (t_range->y = t, 1);
+			return (t_lim->y = t, 1);
 	}
 	return (0);
 }
 
 static int	check_body(t_v3f coeff, t_ray *ray, float h, t_v2f *t)
 {
-	int		i;
 	t_v3f	p;
 	float	t0;
 	float	t1;
 
 	if (!solve_quadratic(&coeff, &t0, &t1))
 		return (0);
-	i = 0;
-	while (i < 2)
+	if (t0 > t->x && t0 < t->y)
 	{
-		if (t0 > t->x && t0 < t->y)
-		{
-			p = v3f_add(ray->origin, v3f_scale(ray->direction, t0));
-			if (p.y >= -h * .5f && p.y <= h * .5f)
-				return (t->y = t0, 1);
-		}
-		t0 = t1;
-		++i;
+		p = v3f_add(ray->origin, v3f_scale(ray->direction, t0));
+		if (p.y >= -h * .5f && p.y <= h * .5f)
+			return (t->y = t0, 1);
 	}
 	return (0);
 }
@@ -103,10 +96,9 @@ int	cylinder_intersect(t_obj *obj, t_ray *ray, t_v2f t, float *dst)
 	h = obj->u_shape.cy.height * .5f;
 	l_ray.origin = mul_v3_m4x4(ray->origin, obj->t.to_obj);
 	l_ray.direction = mul_dir_m4x4(ray->direction, obj->t.to_obj);
-
 	if ((intersect_cylinder_body(obj, &l_ray, &t)
-		| intersect_cylinder_disc(r, h, &l_ray, &t)
-		| intersect_cylinder_disc(r, -h, &l_ray, &t)) == 1)
+			| intersect_cylinder_disc(r, h, &l_ray, &t)
+			| intersect_cylinder_disc(r, -h, &l_ray, &t)) == 1)
 		return (*dst = t.y, 1);
 	return (0);
 }
