6692caf5f5350b78da0ed8d5c12d10a29cce6f36
Jose Boon <jboon@student.codam.nl>
2025-07-31 19:32:33 +0200

    Forgot a file

--- src/render/procedural_texturing.c
+++ src/render/procedural_texturing.c
@@ -6,7 +6,7 @@
 /*   By: jboon <jboon@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/10 23:49:31 by jboon         #+#    #+#                 */
-/*   Updated: 2025/07/31 16:23:20 by jboon         ########   odam.nl         */
+/*   Updated: 2025/07/31 19:29:53 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ t_v3f	noise_pattern(const t_v2f *texcoord, const t_tex *tex, t_v3f col_a)
 
 	point = v2f_mul_v3f(*texcoord, tex->scale_rot);
 	return (v3f_lerp(
-		col_a, tex->col, (tex->fp_perlin(point, &tex->p_data) + 1.0f) * 0.5f));
+			col_a, tex->col, (tex->fp_perlin(point, &tex->p_data) + 1.0f)
+			* 0.5f));
 }
