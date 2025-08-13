7e3e0a1cd63af45d794f701f5b65b3971b45a446
josejboon <josejboon@gmail.com>
2025-08-13 22:02:40 +0200

    Removed unused headers from material.c

--- src/parser/elements/material.c
+++ src/parser/elements/material.c
@@ -6,15 +6,12 @@
 /*   By: jboon <jboon@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/05 14:04:02 by jboon         #+#    #+#                 */
-/*   Updated: 2025/07/31 22:58:43 by jboon         ########   odam.nl         */
+/*   Updated: 2025/08/13 22:01:32 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
-#include "rt_math.h"
-#include "scene.h"
 #include "material.h"
-#include "libft.h"
 
 #define N 4
 
