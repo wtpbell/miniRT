aec92c627735b6ed9a8a9a7ec1767b07353b4287
josejboon <josejboon@gmail.com>
2025-08-20 10:23:25 +0200

    Replaced setting the pixel colors by using mlx_put_pixel and added rt_get_pixel for avoiding the endian problem

--- include/color.h
+++ include/color.h
@@ -3,16 +3,17 @@
 /*                                                        ::::::::            */
 /*   color.h                                            :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: bewong <bewong@student.codam.nl>             +#+                     */
+/*   By: jboon <jboon@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/08 19:16:29 by jboon         #+#    #+#                 */
-/*   Updated: 2025/08/08 16:27:02 by bewong        ########   odam.nl         */
+/*   Updated: 2025/08/20 10:18:12 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLOR_H
 # define COLOR_H
 
+# include "MLX42/MLX42.h"
 # include "vector.h"
 # include <stdint.h>
 
@@ -25,14 +26,15 @@
 
 typedef uint32_t	t_col32;
 
-t_col32	init_col32(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
-int		get_r(t_col32 color);
-int		get_g(t_col32 color);
-int		get_b(t_col32 color);
-int		get_a(t_col32 color);
-t_v3f	col32_to_v3f(t_col32 color);
-t_col32	v3f_to_col32(t_v3f color);
-t_col32	col32_lerp(t_col32 a, t_col32 b, float t);
-t_v3f	v3f_apply_gamma(t_v3f color, float gamma);
+t_col32		init_col32(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
+int			get_r(t_col32 color);
+int			get_g(t_col32 color);
+int			get_b(t_col32 color);
+int			get_a(t_col32 color);
+t_v3f		col32_to_v3f(t_col32 color);
+t_col32		v3f_to_col32(t_v3f color);
+t_col32		col32_lerp(t_col32 a, t_col32 b, float t);
+t_v3f		v3f_apply_gamma(t_v3f color, float gamma);
+uint32_t	rt_get_pixel(mlx_image_t *img, uint32_t x, uint32_t y);
 
 #endif
