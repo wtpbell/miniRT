1b4aed207db61dbbe49c4e973df1e3ea50ea0b0a
josejboon <josejboon@gmail.com>
2025-08-24 16:15:02 +0200

    Temp additional argument for testing mallocs in main

--- src/ft_wrapper.c
+++ src/ft_wrapper.c
@@ -1,7 +1,7 @@
 /**
  * @file ft_wrapper.c
  * @author josejboon
- * @brief Mock (wrap) function calls.
+ * @brief Mock (wrap) function calls. (man ld)
  * Compile using the -Wl,--wrap= flag (comma separate the --wraps for more).
  * gcc -Wl,--wrap=malloc,--wrap=write main.c ft_wrapper.c libft.a
  * === Alternatively use macros ===
@@ -18,41 +18,26 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 int	__malloc_count = 700;
-int __write_count = 10;
 
 void *__real_malloc(size_t size);
 
 void *__wrap_malloc(size_t size)
 {
 	if (__malloc_count-- > 0)
-	{
-		// printf("%d allocations left\n", __malloc_count);
 		return (__real_malloc(size));
-	}
-	dprintf(STDERR_FILENO, "Malloc: denied %zu bytes\n", size);
+	errno = 12;
 	return (NULL);
 }
 
-void	*__real_calloc(size_t n, size_t a);
+void	*__real_calloc(size_t elements, size_t size);
 
-void	*__wrap_calloc(size_t n, size_t a)
+void	*__wrap_calloc(size_t elements, size_t size)
 {
 	if (__malloc_count-- > 0)
-	{
-		// printf("%d allocations left\n", __malloc_count);
-		return (__real_calloc(n, a));
-	}
-	dprintf(STDERR_FILENO, "Calloc: denied %zu bytes\n", n * a);
+		return (__real_calloc(elements, size));
+	errno = 12;
 	return (NULL);
 }
-
-// ssize_t __real_write(int fd, void *buf, size_t n);
-
-// ssize_t __wrap_write(int fd, void *buf, size_t n)
-// {
-// 	if(__write_count-- > 0)
-// 		return (__real_write(fd, buf, n));
-// 	return (-1);
-// }
