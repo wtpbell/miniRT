089d33ad1a32ae090e1f0568fc5a7cef15de52cc
Jose Boon <jboon@student.codam.nl>
2025-08-28 17:17:08 +0200

    Restore mul of surface color with lightning

--- src/render/rt_material.c
+++ src/render/rt_material.c
@@ -6,7 +6,7 @@
 /*   By: jboon <jboon@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/02 11:16:23 by bewong        #+#    #+#                 */
-/*   Updated: 2025/08/27 20:34:27 by jboon         ########   odam.nl         */
+/*   Updated: 2025/08/28 17:14:27 by jboon         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ t_v3f	handle_dielectric(t_scene *sc, t_ray_hit *hit, uint32_t depth)
 		ior = hit->obj->r.mat->diel.ir;
 	indirect = blend_color(sc, hit, depth, ior);
 	if (hit->obj->r.mat->diel.transmittance < 0.99f)
-		direct_light = compute_lighting(hit, sc);
+		direct_light = v3f_mul(hit->hit_color, compute_lighting(hit, sc));
 	if (hit->obj->r.mat->diel.transmittance >= 0.99f)
 		return (v3f_clampf01(indirect));
 	if (hit->obj->r.mat->diel.transmittance <= 0.01f)
@@ -60,7 +60,7 @@ t_v3f	handle_lambertian(t_scene *scene, t_ray_hit *hit_info)
 	}
 	lighting = compute_lighting(hit_info, scene);
 	hit_info->normal = org_norm;
-	return (v3f_clampf01(lighting));
+	return (v3f_clampf01(v3f_mul(hit_info->hit_color, lighting)));
 }
 
 t_v3f	handle_metal(t_scene *sc, t_ray_hit *hit, uint32_t depth)
